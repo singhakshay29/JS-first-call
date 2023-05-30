8863e94833c9e77dc9d8c6450755812006173ac9
singhakshay29 <akshay2898.as@gmail.com>
2023-05-30 16:07:34 +0530

    Js-first-call

---
+++ DOM/DOM.js
@@ -0,0 +1,28 @@
+//web pages--->are document (an html page)
+// Dots per Inch
+//Browser is host which provide some envirnoment
+//DOM ---> is resposible for view
+//BOM Browser object Model created by google people
+//BOM provides -Local storage,location,debugging tools
+// ------------Browser/window-----------
+// |              |                    |
+// js           BOM                   DOM
+
+//DOM has ui structure 1 html 
+//BOM has Location, Local storage ,Debugging tools
+
+
+//window contains all properties of BOM and DOM
+//DOM and document both are same
+// const a=window.document
+// if we do this document contain hole body
+//  to se the object 
+//  console.dir(a);
+//  now we can see the objects of a
+//  object is collection of key and value 
+//  for every tab we open their we are creating new window object
+
+
+//  CSSOM-----> Cssobject value model
+
+//  Rendor Tree -----> combination of DOM and CSSOM
---
+++ DOM/Selectors/script.js
@@ -0,0 +1,13 @@
+/*
+........Selectors
+*/
+// 1. getElementsByTagName
+
+
+// let para=document.getElementsByTagName("p");
+// console.log(para);
+
+//2.getElementsById
+
+let idname=document.getElementById("para");
+console.log(idname);
