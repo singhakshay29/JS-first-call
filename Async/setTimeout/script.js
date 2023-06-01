d300b80489b06c79f552a3258b5ee86ed52cd071
singhakshay29 <akshay2898.as@gmail.com>
2023-06-01 18:43:02 +0530

    Js-first-call

---
+++ Async/setTimeout/script.js
@@ -0,0 +1,27 @@
+//setTime Out
+
+
+// console.log("hello world")
+
+// setTimeout(()=>{
+//     console.log('it monday');
+// },0);
+
+
+// console.log('a');
+// setTimeout(()=>{
+//     console.log('b');
+// },0);
+// console.log('c');
+
+
+
+
+
+//console.log('a');//it goes to stack
+// setTimeout(()=>{
+//     console.log('b');//--->it goes to web api where is timer start
+// },3000);//as sooon as it comes to 0--> it goes to task queue
+//their is event loop which check if stack is empty then it allow the task queue entries to stack
+// console.log('c');
+
