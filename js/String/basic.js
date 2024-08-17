195a2436913ae29373ce13977bedc8af95d783e7
akshaysingh29 <akshay.singh@uniproworld.com>
2024-08-17 17:50:49 +0530

    some more ques

--- js/String/basic.js
+++ js/String/basic.js
@@ -40,3 +40,90 @@ function sum(a,b){
         }
     }
 }
+
+console.log('4' * 6 + 10);
+
+console.log('5' - '3');
+
+console.log(NaN === NaN);
+
+
+let str='ak ak a aks aksh ';
+//output = akshay longest word;
+
+
+function findlongestStr(str){
+let newStr=str.split(" ");
+let max=newStr[0].length;//4
+let result=newStr[0];
+for(let i = 1;i<newStr.length;i++){
+if(max<newStr[i].length){
+result=newStr[i];
+}
+}
+return result
+}
+
+console.log(findlongestStr(str));
+
+console.log(12**12);
+
+
+function test(){
+    salary=1200;
+    console.log(salary);
+    }
+    
+    
+    test();
+
+    console.log({} == {});
+    
+    
+    let a={
+        name:"akshay"
+        }
+        
+        let b={
+        ...a
+        }
+        
+        b.name="raja";
+        
+        console.log(b.name);
+
+        // let a=10;
+
+        // let b=new Number(10);
+        
+        // let c=10;
+        
+        // console.log(b===c);
+        
+        
+        // function test(record){
+        //     if(record=={age:28}){
+        //     console.log("Hello");
+        //     }else if(record === {age:28}){
+        //     console.log("World");
+        //     }else {
+        //     console.log("No record")
+        //     }
+        //     }
+            
+        //     test({age:28});
+        
+//         var a={}
+// var b={key:'b'};
+// var c={key:'c'};
+
+
+// a[b]=600;
+// b[c]=700;
+// b[b]=400;
+
+
+// console.log(a[c]);
+// console.log(a[b]);
+// console.log(b[b]);
+// console.log(b[c]);
