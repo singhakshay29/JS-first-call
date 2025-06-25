15e845be2e465a1975ce0aa848f6ab816e7eb19d
akshaysingh29 <akshay2898.as@gmail.com>
2025-06-25 22:33:06 +0530

    advance start

--- 2025/Array/script.js
+++ 2025/Array/script.js
@@ -134,12 +134,31 @@ function leftRotation(arr){
     arr[arr.length-1]=temp;
 }
 
-//right rotation by 2
+//right rotation by 1
 
 arr=[2,5,7,9,32,54];
 function rightRotation(arr){
     let temp = arr[arr.length-1];
-    for(let i=1;i<arr.length-1;i++){
-        arr[i+1]=arr[i];
+    for(let i=arr.length-1;i<0;i--){
+        arr[i]=arr[i-1];
     }
+    arr[0]=temp;
 }
+//level II
+
+
+//left rotation by k
+function rotationByK(arr,k){
+    k=k%arr.length;
+    for(let i=0;i<k;i++){
+        let temp=arr[0];
+        for(let j=0;j<arr.length-1;j++){
+            arr[j]=arr[j+1];
+        }
+        arr[arr.length-1]=temp;
+    }
+    return arr;
+}
+
+console.log(rotationByK(arr,k));
+
